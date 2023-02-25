1b2ef8054fcc07e08c91ce4e84d08c99087047b7
xwebz <126299896+xwebz@users.noreply.github.com>
2023-02-24 23:02:41 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-const form = document.getElementById('myForm');
+const form = document.getElementById('myForm');            // Language / Location Menu
+const change = document.getElementById('preferences-btn'); // Button within the Invitation to reopen Menu
 
+form.style.visibility = 'visible';  // Form is what the user sees first
+change.style.visibility = 'hidden'; // Hidden button to reopen Menu, when Menu is visible
+
+// When Menu Preferences Summited, Hide Form and Show Button to reopen it
 form.addEventListener('submit', function(event) {
     event.preventDefault();
-    form.style.display = 'none';
+    form.style.visibility = 'hidden';
+    change.style.visibility = 'visible';
+});
+
+// When Button to reopen Menu clicked, hide button and reopen Menu
+change.addEventListener('click', function(event) {
+    event.preventDefault();
+    change.style.visibility = 'hidden';
+    form.style.visibility = 'visible';
 });
